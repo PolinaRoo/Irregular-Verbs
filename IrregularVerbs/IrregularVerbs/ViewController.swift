29869e981b61d2a810c2d40b778c550bf6804909
PolinaRoo <polina.roo10@gmail.com>
2023-07-28 20:04:30 +0300

    Initial commit

---
+++ IrregularVerbs/IrregularVerbs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IrregularVerbs
+//
+//  Created by Polina Tereshchenko on 28.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
