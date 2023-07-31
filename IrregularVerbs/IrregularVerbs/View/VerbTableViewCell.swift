be4568952d46b80270f3622944cce197a5869857
PolinaRoo <polina.roo10@gmail.com>
2023-07-31 14:02:32 +0300

    no message

---
+++ IrregularVerbs/IrregularVerbs/View/VerbTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  VerbTableViewCell.swift
+//  IrregularVerbs
+//
+//  Created by Polina Tereshchenko on 30.07.2023.
+//
+
+import UIKit
+
+final class VerbTableViewCell: UITableViewCell {
+    @IBOutlet private weak var infinitiveLabel: UILabel!
+    @IBOutlet private weak var pastSimpleLabel: UILabel!
+    @IBOutlet private weak var participleLabel: UILabel!
+    @IBOutlet private weak var translationLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+    
+    func configure(for verb: Verb) {
+        infinitiveLabel.text = verb.infinitive
+        pastSimpleLabel.text = verb.pastSimple
+        participleLabel.text = verb.participle
+        translationLabel.text = verb.translation
+    }
+}
